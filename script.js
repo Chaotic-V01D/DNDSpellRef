6fbd0901444e70c02c08b3fc350f4fb27e5de61d
Chaotic-V01D <87347377+Chaotic-V01D@users.noreply.github.com>
2023-05-02 11:48:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let spellClasses = [];
-let dmgTypes = [];
-let Spell = function(name, level, classOpt, dmg, description){
+let spellClasses = [];//Classes with access to spells (sorted alphabetically)
+let dmgTypes = [];//Damage types of spells (sorted alphabetically)
+let schools = [];
+let Spell = function(name, dmg, school, level, ritual, conc, source, classOpt, description){
   this.name=name;//String, name of the spell
-  this.level=level;//Integer, level of the spell
-  this.classOpt=classOpt;//Int Array, the indexes of the spellClasses array that the spell is available to
   this.dmg=dmg;//Int Array, the indexes of the dmg type that the spell does (an array bc some have multiple i.e. Chromatic Orb)
+  this.school=school;//Integer, the index of the schools array that the spell is in
+  this.level=level;//Integer, level of the spell
+  this.conc = conc;//Boolean,  is the spell concentration
+  this.source = source;//Boolean, false for PHB, true for DMG
+  this.classOpt=classOpt;//Int Array, the indexes of the spellClasses array that the spell is available to
   this.description=description;//String, the description of the spell in the PHB or DMG
+}
+let spellList = []
+spellList.push(new Spell("Acid Splash", 0, [0], [0], "A splash of acid erupts from a place of your choosing"))
+
+function sortSpells(method){
+  
 }
