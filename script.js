58cc7f0f21aa798a7679fa37cdd4b9ef1717fb59
Chaotic-V01D <87347377+Chaotic-V01D@users.noreply.github.com>
2023-05-04 11:51:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let spellClasses = ["Bard","Cleric","Druid","Paladin","Sorceror","Warlock","Wiza
 let dmgTypes = ["Acid", "Bludgeoning", "Cold", "Fire", "Force", "Lightning", "Necrotic", "Piercing", "Poison", "Psychic", "Radiant", "Slashing", "Thunder"];//Damage types of spells (sorted alphabetically)
 let schools = ["Abjuration", "Conjuration", "Divination", "Enchantment", "Evocation", "Illusion", "Necromancy", "Transmutation"];
 let castTimeStamps=["1 reaction", "1 bonus action", "1 action", "1 minute", "10 minutes", "1 hour", "8 hours", "1 day"]
-let Spell = function(name, dmg, school, level, ritual, conc, castTime, source, classOpt, description){
+let Spell = function(name, dmg, school, level, ritual, conc, castTime, source, louisaYummy, classOpt, description){
   this.name=name;//String, name of the spell
   this.dmg=dmg;//Int Array, the indexes of the dmg type that the spell does (an array bc some have multiple i.e. Chromatic Orb)
   this.school=school;//Integer, the index of the schools array that the spell is in
@@ -10,11 +10,12 @@ let Spell = function(name, dmg, school, level, ritual, conc, castTime, source, c
   this.conc = conc;//Boolean,  is the spell concentration
   this.castTime = castTime//Integer, the index of the castTimeStamps array that the spell requires
   this.source = source;//Boolean, false for PHB, true for DMG
+  this.louisaYummy= louisaYummy;//Boolean, does Louisa think that this spell is yummy?
   this.classOpt=classOpt;//Int Array, the indexes of the spellClasses array that the spell is available to
   this.description=description;//String, the description of the spell in the PHB or DMG
 }
 let spellList = []
-spellList.push(new Spell("Acid Splash", 0, 1, 0, 0, false, 0, [4,6],"You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a Dexterity saving throw or take 1d6 acid damage."))
+spellList.push(new Spell("Acid Splash", 0, 1, 0, 0, false, 0, true, [4,6],"You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a Dexterity saving throw or take 1d6 acid damage."))
 
 function sortSpells(method){
   let sorted = false;
