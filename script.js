5d0667af05f3109ca4c07beffa99c3f09f88c22a
Chaotic-V01D <87347377+Chaotic-V01D@users.noreply.github.com>
2023-05-12 11:10:27 -0500

    Blade Ward ex

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let spellClasses = ["Bard","Cleric","Druid","Paladin","Sorceror","Warlock","Wizard"];//Classes with access to spells (sorted alphabetically)
-let dmgTypes = ["Acid", "Bludgeoning", "Cold", "Fire", "Force", "Lightning", "Necrotic", "Piercing", "Poison", "Psychic", "Radiant", "Slashing", "Thunder"];//Damage types of spells (sorted alphabetically)
+let spellClasses = ["Artificer", "Bard","Cleric","Druid","Paladin","Sorceror","Warlock","Wizard"];//Classes with access to spells (sorted alphabetically)
+let dmgTypes = ["N/A", "Acid", "Bludgeoning", "Cold", "Fire", "Force", "Lightning", "Necrotic", "Piercing", "Poison", "Psychic", "Radiant", "Slashing", "Thunder"];//Damage types of spells (sorted alphabetically)
 let schools = ["Abjuration", "Conjuration", "Divination", "Enchantment", "Evocation", "Illusion", "Necromancy", "Transmutation"];
 let castTimeStamps=["1 reaction", "1 bonus action", "1 action", "1 minute", "10 minutes", "1 hour", "8 hours", "1 day"]
 let spellSource = ["PHB", "TCE", "XGE", "GGR", "SCAG", "EEPC", "EGW", "AI", "ID:RF", "SCC", "SAS", "LLK", "FTD"]
@@ -16,8 +16,8 @@ let Spell = function(name, dmg, school, level, ritual, conc, castTime, source, l
   this.description=description;//String, the description of the spell in the PHB or DMG
 }
 let spellList = []
-spellList.push(new Spell("Acid Splash", 0, 1, 0, 0, false, 0, 0, [4,6],"You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a Dexterity saving throw or take 1d6 acid damage."))
-
+spellList.push(new Spell("Acid Splash", [1], 1, 0, false, 0, 0, [4,6],"You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a Dexterity saving throw or take 1d6 acid damage."))
+spellList.push(new Spell("Blade Ward", [0], 0, false, false, 2, 0, true, [1, 5, 6, 7], "description"))
 function sortSpells(method){
   let sorted = false;
   while(!sorted){
